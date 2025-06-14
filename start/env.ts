170c2865ddf90d9e963233606df2db1e2b53f918
Mel-aine <melaineevans7@gmail.com>
2025-06-14 07:11:57 +0100

    commit

--- start/env.ts
+++ start/env.ts
@@ -43,8 +43,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   |----------------------------------------------------------
   */
 
-  CLOUDINARY_CLOUD_NAME: Env.schema.string(),
-  CLOUDINARY_API_KEY: Env.schema.string(),
-  CLOUDINARY_API_SECRET: Env.schema.string(),
+  // CLOUDINARY_CLOUD_NAME: Env.schema.string(),
+  // CLOUDINARY_API_KEY: Env.schema.string(),
+  // CLOUDINARY_API_SECRET: Env.schema.string(),
 
 })
