1b8a6dca7734bd17cfc25d0114c882ba191f2e8a
NDE Ferry <ndeferry82@gmail.com>
2025-07-07 08:54:16 +0100

    ...

--- app/controllers/roles_controller.ts
+++ app/controllers/roles_controller.ts
@@ -1,12 +1,13 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 
 import Role from '#models/role'
 import CrudService from '#services/crud_service'
 import CrudController from './crud_controller.js'
+import Service from '#models/service'
 
 // import type { HttpContext } from '@adonisjs/core/http'
-const roleService = new CrudService(Role)
+//const roleService = new CrudService(Role)
 
 export default class RolesController extends CrudController<typeof Role> {
   constructor() {
