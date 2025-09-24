46e4bfb4737e0c06db9537e20ef73e738c1fd91f
Mel-aine <melaineevans7@gmail.com>
2025-09-24 16:25:03 +0100

    commit

--- database/migrations/1756907558204_create_add_fields_to_reservations_table.ts
+++ database/migrations/1756907558204_create_add_fields_to_reservations_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.alterTable(this.tableName, (table) => {
       table.boolean('send_mail').defaultTo(0)
       table.boolean('check_out_mail').defaultTo(0)
-      table.boolean('thank_you_email_to_guest').defaultTo(0)
+      table.boolean('thank_you_email').defaultTo(0)
       table.boolean('supress_rate').defaultTo(0)
       table.boolean('access_guest_portal').defaultTo(0)
     })
@@ -17,7 +17,7 @@ export default class extends BaseSchema {
     this.schema.alterTable(this.tableName, (table) => {
       table.boolean('send_mail').defaultTo(0)
       table.boolean('check_out_mail').defaultTo(0)
-      table.boolean('thank_you_email_to_guest').defaultTo(0)
+      table.boolean('thank_you_email').defaultTo(0)
       table.boolean('supress_rate').defaultTo(0)
       table.boolean('access_guest_portal').defaultTo(0)
     })
