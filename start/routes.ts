38932668a9b9616d1291322194560b1bae58e02f
Your Name <styvesdaudet@gmail.com>
2025-11-15 09:07:16 +0100

    test

--- start/routes.ts
+++ start/routes.ts
@@ -588,26 +588,7 @@ router
       router.put('/activity-logs/:id', activityLogsController.update.bind(activityLogsController))
     })
 
-    // TODO: Implement ReservationServiceProductsController
-    // Reservation service product routes will be added once the controller is implemented
 
-
-
-    // TODO: Implement PaymentsController
-    // Payment routes will be added once the controller is implemented
-
-
-    // router.group(() => {
-    //   router.post('/schedules', schedulesController.create.bind(SchedulesController))
-    //   router.get('/schedules', schedulesController.lister.bind(SchedulesController))
-    // })
-
-    // router.group(() => {
-    //   router.get(
-    //     '/services/:serviceId/products/grouped',
-    //     serviceProductsController.getGroupedByAccommodationType.bind(ServiceProductsController)
-    //   )
-    // })
 
     router.group(() => {
       router.get('/assigmentUser', assigmentUsersController.list.bind(assigmentUsersController))
